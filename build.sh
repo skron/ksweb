cc1b51893171062b13cfceca19bc75666c3afffa
Filip Oščádal <filip@mxd.cz>
2014-04-02 17:22:46 +0200

    template builder

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+T='templates/'
+
+cat $T"header.html" $T"index.html" $T"footer.html" > index.html
+cat $T"header.html" $T"klimatizace-acond-byty.html" $T"footer.html" > klimatizace-acond-byty.html
+cat $T"header.html" $T"klimatizace-acond.html" $T"footer.html" > klimatizace-acond.html
+cat $T"header.html" $T"kontakt.html" > kontakt.html
+cat $T"header.html" $T"o-nas.html" $T"footer.html" > o-nas.html
+cat $T"header.html" $T"sluzby.html" $T"footer.html" > sluzby.html
